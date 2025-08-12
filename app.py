19c80f2161271217792dd9e4bb1ddc83c13caf60
Ellis Lunayo <ellislunayo@gmail.com>
2025-08-12 21:00:59 +0300

    added a course path

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask (__name__)
 #added a app route using the decorator @app.route
 @app.route('/')
 def index ():
-    return "<p> Welcome to Tara School </p>"
+    return " Welcome to Tara School"
 
 #adding new routes
 @app.route("/courses")
@@ -14,15 +14,22 @@ def courses ():
     return "This is the courses page"
 
 #creating path parameters
-def course_details():
-    return f"This is course: {Computer_Science} "
+@app.route("/courses/<course_id>")
+def course_details(course_id):
+    return f"This is course: {course_id} "
+
+#add a path
+@app.route("/courses/<path: course_file>")
+def coure_file(course_file):
+    return f"Currently serving : {coure_file}"
+
 
 @app.route("/about")
 def about ():
     return "What to know about us"
 
 @app.route("/contact")
-def contact():
+def contact ():
     return "Contact Us Here"
 
 
